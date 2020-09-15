b6c33a023f39e7f2bc469d2638191f54f9df8a2c
Süleyman BABAOĞLU <suleymanbabaoglu@hotmail.com>
2020-09-16 01:33:57 +0300

    1

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeLayout from "./layouts/_Layout";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 Vue.use(VueRouter);
 
@@ -10,7 +12,9 @@ export const routes = [
     path: "/",
     component: HomeLayout,
     children: [{ path: "/", component: Home }]
-  }
+  },
+  { path: "/login", component: Login },
+  { path: "/register", component: Register }
 ];
 
 export const router = new VueRouter({
